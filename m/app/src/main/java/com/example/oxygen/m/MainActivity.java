b7a3913f079a089f3cb27a4ea565920c43bc2b0c
hailt <xaviertz021@gmail.com>
2018-10-11 17:34:29 +0700

    hello cc

--- m/app/src/main/java/com/example/oxygen/m/MainActivity.java
+++ m/app/src/main/java/com/example/oxygen/m/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.oxygen.m;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -12,29 +14,52 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<detail> danhsach;
-    private JsonArray sdanhsach = new JsonArray();
-    private String danhSachTam;
-    private Button
+
+    TextView tvCity, tvTemp,tvHumid, tvWind, tvCloud, tvDate;
+    ArrayList<WeatherObject> listThoiTiet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getControl();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        getDataUrl();
+        getDataUrl("1","1");
     }
+
+    protected void getControl()
+    {
+        tvDate = (TextView) findViewById(R.id.DataTimeUpdate);
+        tvCity = (TextView) findViewById(R.id.City);
+        tvTemp = (TextView) findViewById(R.id.temp);
+        tvHumid = (TextView) findViewById(R.id.humidity);
+        tvWind = (TextView) findViewById(R.id.wind);
+        tvCloud = (TextView) findViewById(R.id.clouds);
+        listThoiTiet = new ArrayList<WeatherObject>();
+    }
+
+
 
     @Override
     protected void onStop() {
@@ -54,9 +79,11 @@ public class MainActivity extends AppCompatActivity {
 
     // Lấy thông tin từ OpenWeatherMap
     // Sử dụng kinh độ và vĩ độ để lấy dữ liệu vị trí thời tiết
-    public void getDataUrl() {
-        String url = "https://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=b1b15e88fa797225412429c1c50c122a1";
-        float lat, lon;
+    public void getDataUrl(String lat, String lon) {
+        String url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=be8d3e323de722ff78208a7dbb2dcd6f";
+        //Viết nốt check lat lon trong dãy
+
+
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -65,8 +92,53 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        danhSachTam = response.toString();
-                        Toast.makeText(MainActivity.this, "Tải nội dung thành công\n" + response.substring(0, 500), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Tải nội dung thành công\n" + response.toString(), Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            //lấy tên thành phố
+                            String name = jsonObject.getString("name");
+                            tvCity.setText("Thành phố: " + name);
+
+                            //lấy ngày tháng update
+                            String day = jsonObject.getString("dt");
+                            //xử lý format ngày tháng từ JSON
+                            long l = Long.valueOf(day);
+                            Date date = new Date(l * 1000L);
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE yyyy-MM-dd");
+                            String Day = simpleDateFormat.format(date);
+                            tvDate.setText(Day);
+
+                            //lấy nhiệt độ, độ ẩm
+                            JSONObject jsonObjectMain = jsonObject.getJSONObject("main");
+                            String nhietDo = jsonObjectMain.getString("temp");
+                            String doAm = jsonObjectMain.getString("humidity");
+
+                            Double a = Double.valueOf(nhietDo);
+                            String NhietDo = String.valueOf(a.intValue());
+                            tvTemp.setText("Nhiệt độ: " + NhietDo + "°C");
+                            tvHumid.append("Độ ẩm: " + doAm + "%");
+
+                            //lấy độ gió, mây
+                            JSONObject jsonObjectWind = jsonObject.getJSONObject("wind");
+                            String gio = jsonObjectWind.getString("speed");
+                            tvWind.setText("Độ gió: " + gio + "m/s");
+
+                            JSONObject jsonObjectCloud = jsonObject.getJSONObject("clouds");
+                            String may = jsonObjectCloud.getString("all");
+                            tvCloud.setText("Mây: " + may + "%");
+
+                            for (WeatherObject wo:listThoiTiet)
+                            {
+                                if(wo.getLat().equals(lat)==1 && wo.getLon().equals(lon)==1)
+                                {
+                                    WeatherObject wObject = new WeatherObject(Day,name,NhietDo,doAm,gio,may);
+                                    listThoiTiet.add(wObject);
+                                }
+                            }
+                        } catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -75,20 +147,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         queue.add(stringRequest);
-        listContext();
-    }
-
-    //đưa dữ liệu vào danh sách lưu trữ.
-    public void listContext() {
-        Type type = new TypeToken<detail>() {
-        }.getType();
-        Gson gson = new Gson();
-        List<detail> danh_sach = gson.fromJson(danhSachTam, type);
-        for (detail i : danh_sach) {
-            detail item_detail = new detail(i.getTenThanhPho(), i.getGio(), i.getMay(), i.getDoAm(),
-                    i.getDoAm(), i.getLuongMua(), i.getTime_update());
-            danhsach.add(item_detail);
-        }
     }
 
 }
---
+++ m/app/src/main/java/com/example/oxygen/m/WeatherObject.java
@@ -0,0 +1,105 @@
+package com.example.oxygen.m;
+
+public class WeatherObject {
+    private String lat,lon, date, city, temp, humid, wind, cloud;
+
+    public WeatherObject(String lat, String lon, String date, String city, String temp, String humid, String wind, String cloud) {
+        this.lat = lat;
+        this.lon = lon;
+        this.date = date;
+        this.city = city;
+        this.temp = temp;
+        this.humid = humid;
+        this.wind = wind;
+        this.cloud = cloud;
+    }
+
+    public WeatherObject(String lat, String lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public WeatherObject(String date, String city, String temp, String humid, String wind, String cloud) {
+        this.date = date;
+        this.city = city;
+        this.temp = temp;
+        this.humid = humid;
+        this.wind = wind;
+        this.cloud = cloud;
+    }
+
+    public WeatherObject() {
+        this.date = "";
+        this.city = "";
+        this.temp = "";
+        this.humid = "";
+        this.wind = "";
+        this.cloud = "";
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getHumid() {
+        return humid;
+    }
+
+    public void setHumid(String humid) {
+        this.humid = humid;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public String getCloud() {
+        return cloud;
+    }
+
+    public void setCloud(String cloud) {
+        this.cloud = cloud;
+    }
+
+
+}
